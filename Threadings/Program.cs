9c0145c47e50d29dac8abc4dc131bbc3b442ef15
phytax <otto@planet.ms>
2018-12-29 18:03:58 +0100

    otzher way round

--- Threadings/Program.cs
+++ Threadings/Program.cs
@@ -7,19 +7,16 @@ namespace Threadings
     internal class Program
     {
         private static long _sum;
-        private static readonly object SumLock = new object();
-        private const int Length = 1_000_000;
+        private static readonly object _sumLock = new object();
+        private static int _length = 1_000_000;
 
         private static void Main()
         {
-            var sw = new Stopwatch();
-            sw.Start();
+                       var t1 = new Thread(Locked1);
+                       var t2 = new Thread(Locked2);
 
-//            var t1 = new Thread(Locked1);
-//            var t2 = new Thread(Locked2);
-
-             var t1 = new Thread(Lockless1);
-             var t2 = new Thread(Lockless2);
+            // var t1 = new Thread(Lockless1);
+            // var t2 = new Thread(Lockless2);
 
             t1.Start();
             t2.Start();
@@ -27,12 +24,12 @@ namespace Threadings
             t1.Join();
             t2.Join();
 
-            Console.WriteLine($"{sw.ElapsedTicks,10} | Result: {_sum, 30}");
+            Console.WriteLine(_sum);
         }
 
         private static void Locked1()
         {
-            lock (SumLock)
+            lock (_sumLock)
             {
                 Lockless1();
             }
@@ -40,7 +37,7 @@ namespace Threadings
 
         private static void Lockless1()
         {
-            for (var i = 1; i <= Length / 2; i++)
+            for (var i = 1; i <= _length / 2; i++)
             {
                 _sum += i;
             }
@@ -48,7 +45,7 @@ namespace Threadings
 
         private static void Locked2()
         {
-            lock (SumLock)
+            lock (_sumLock)
             {
                 Lockless2();
             }
@@ -56,7 +53,7 @@ namespace Threadings
 
         private static void Lockless2()
         {
-            for (var i = Length / 2 + 1; i <= Length; i++)
+            for (var i = _length / 2 + 1; i <= _length; i++)
             {
                 _sum += i;
             }
